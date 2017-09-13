7389ee6e439bbeab7e5946273782ac179bc14ed9
pro-use <robertprouse@gmail.com>
2017-09-13 22:16:04 +0100

    add omxSocket.js

--- static/omxSocket.js
+++ static/omxSocket.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-    namespace = '/progress';
+
+    namespace = '/position';
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+
+    socket.on('connect', function() {
+                console.log('I\'m connected!');
+            });
+
     socket.on('position', function(msg) {
+        console.log(msg.position);
         $('#progress_bar').html(msg.position);
     });
-});
