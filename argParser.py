009c7ce5ecdb490edde4c3af040945f272a60e4f
pro-use <robertprouse@gmail.com>
2017-09-14 00:24:14 +0100

    spin out arg_parser

---
+++ argParser.py
@@ -0,0 +1,59 @@
+import sys
+import getopt
+import glob
+import os
+from re import escape
+
+
+def setup():
+    directory = "/home/pi/video/"
+    files = glob.glob(directory + '[a-zA-Z0-9]*.*')
+    sync = None
+    audio = 'local'
+    helpstr = '\nomxOnline usage:\n \
+        -h print this help message and exit\n \
+        -f <file>               full path\n \
+        -d <directory> \n \
+        -s [master | slave]     sync mode\n \
+        -o [local | hdmi | alsa]    audio output\n'
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hf:d:s:o:")
+    except getopt.GetoptError:
+        print(helpstr)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(helpstr)
+            sys.exit(2)
+        elif opt == '-d':
+            if not os.path.exists(arg):
+                print('\n%s is not a valid directory\n' % arg)
+                sys.exit(2)
+            if '-f' in opts:
+                print('\nDirectory and file both specified, ignoring directory %s\n' % arg)
+            else:
+                directory = arg
+                files = glob.glob(directory + '[a-zA-Z0-9]*.*')
+        elif opt == '-f':
+            if not os.path.isfile(arg):
+                print('\nCannot find file %s\n' % arg)
+                sys.exit(2)
+            files = [arg]
+        elif opt == '-s':
+            if arg not in ['slave', 'master']:
+                print('\n"%s" is not a valid sync mode, must be either "master" or "slave"\n' % arg)
+                sys.exit(2)
+            sync = arg
+        elif opt == '-o':
+            if arg not in ['local', 'hdmi', 'alsa']:
+                print('\n"%s" is not a valid audio output, must be either "local", "hdmi" or "alsa"\n' % arg)
+                sys.exit(2)
+            audio = arg
+        for f in range(0, len(files)):
+            files[f] = escape(files[f])
+        if len(files) > 1:
+            print('\ncannot sync multiple files,'
+                  ' please specify a file or choose a different directory in order to sync\n')
+            sys.exit(2)
+    return directory, files, sync, audio
--- omxOnline.py
+++ omxOnline.py
@@ -1,91 +1,28 @@
-import sys
-import getopt
-import glob
-import os
+
 import time
-from re import escape
+from argParser import setup
 from omxplayer import OMXPlayer
 from flask import Flask, render_template, session, request, jsonify, abort
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 from threading import Lock
 
-
-def setup():
-    directory = "/home/pi/video/"
-    files = glob.glob(directory + '[a-zA-Z0-9]*.*')
-    sync = None
-    audio = 'local'
-    helpstr = '\nomxOnline usage:\n \
-        -h print this help message and exit\n \
-        -f <file>               full path\n \
-        -d <directory> \n \
-        -s [master | slave]     sync mode\n \
-        -o [local | hdmi | alsa]    audio output\n'
-
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hf:d:s:o:")
-    except getopt.GetoptError:
-        print(helpstr)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(helpstr)
-            sys.exit(2)
-        elif opt == '-d':
-            if not os.path.exists(arg):
-                print('\n%s is not a valid directory\n' % arg)
-                sys.exit(2)
-            if '-f' in opts:
-                print('\nDirectory and file both specified, ignoring directory %s\n' % arg)
-            else:
-                directory = arg
-                files = glob.glob(directory + '[a-zA-Z0-9]*.*')
-        elif opt == '-f':
-            if not os.path.isfile(arg):
-                print('\nCannot find file %s\n' % arg)
-                sys.exit(2)
-            files = [arg]
-        elif opt == '-s':
-            if arg not in ['slave', 'master']:
-                print('\n"%s" is not a valid sync mode, must be either "master" or "slave"\n' % arg)
-                sys.exit(2)
-            sync = arg
-        elif opt == '-o':
-            if arg not in ['local', 'hdmi', 'alsa']:
-                print('\n"%s" is not a valid audio output, must be either "local", "hdmi" or "alsa"\n' % arg)
-                sys.exit(2)
-            audio = arg
-        for f in range(0, len(files)):
-            files[f] = escape(files[f])
-    return directory, files, sync, audio
-
-
-def setup_sync(sync, files):
-    if sync:
-            if len(files) > 1:
-                print('\ncannot sync multiple files,'
-                      ' please specify a file or choose a different directory in order to sync\n')
-                sys.exit(2)
-            # from omxsync import Broadcaster
-
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='eventlet')
 thread_lock = Lock()
-DIRECTORY, FILES, SYNC, AUDIO = setup()
-print(DIRECTORY, FILES, SYNC, AUDIO)
-PLAYER = OMXPlayer(FILES[0], args=['-o', AUDIO, '--no-osd', '--loop'])
+directory, files, sync, audio = setup()
+player = OMXPlayer(files[0], args=['-o', audio, '--no-osd', '--loop'])
 thread = None
-duration = PLAYER.duration()
+duration = player.duration()
 duration_percent = 100 / duration
 duration_str = time.strftime('%H:%M:%S', time.gmtime(duration))
-filename = PLAYER.get_filename().split('/')[-1]
+filename = player.get_filename().split('/')[-1]
 
 
 def position_thread():
     while True:
         socketio.sleep(1)
-        pos = PLAYER.position()
+        pos = player.position()
         percentage = duration_percent * pos
         pos = time.strftime('%H:%M:%S', time.gmtime(pos))
         socketio.emit('position',
@@ -109,5 +46,5 @@ def connect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
-    PLAYER.stop()
+    socketio.run(app, host='0.0.0.0', debug=True, use_reloader=False)
+    player.stop()
